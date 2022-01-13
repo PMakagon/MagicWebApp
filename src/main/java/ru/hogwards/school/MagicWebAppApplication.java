cbd186f5a8f62e4533476581e13680f7b4b120b3
Пётр Макагон <haggard95@yandex.ru>
2022-01-13 15:58:50 +0300

    Initial commit

---
+++ src/main/java/ru/hogwards/school/MagicWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwards.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicWebAppApplication.class, args);
+    }
+
+}
