2b6d109e53a28a149394ec8a4a1c7f15180adde8
Пётр Макагон <haggard95@yandex.ru>
2022-04-18 16:52:43 +0300

    4.1

--- src/main/java/ru/hogwards/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwards/school/repository/AvatarRepository.java
@@ -1,11 +1,13 @@
 package ru.hogwards.school.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.hogwards.school.domain.Avatar;
 
+import java.util.Collection;
 import java.util.Optional;
 
-public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+public interface AvatarRepository extends PagingAndSortingRepository<Avatar, Long> {
     Optional<Avatar> findByStudentId(Long studentId);
+    Collection<Avatar> findAll();
 }
 
--- src/main/java/ru/hogwards/school/services/AvatarService.java
+++ src/main/java/ru/hogwards/school/services/AvatarService.java
@@ -4,8 +4,11 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.hogwards.school.domain.Avatar;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 
 public interface AvatarService {
     void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException;
     Avatar findAvatar(Long studentId);
+    Collection<Avatar> findAll(int pageNumber, int pageSize);
 }
